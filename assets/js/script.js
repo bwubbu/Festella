a6536a04f35d00d241c17c5f91b7c8aeba49f193
bwubbu <kyrodahero123@gmail.com>
2024-04-22 13:19:36 +0800

    updated profile settings

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,3 +9,21 @@ registerBtn.addEventListener('click', () => {
 loginBtn.addEventListener('click', () => {
     container.classList.remove("active");
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+    const forgotPasswordLink = document.getElementById("forgot-password-link");
+    const signInFormContainer = document.querySelector(".sign-in");
+  
+    forgotPasswordLink.addEventListener("click", function(event) {
+      event.preventDefault();
+      signInFormContainer.innerHTML = `
+        <form>
+          <h1>Change Password</h1>
+          <div class="social-icons"></div>
+          <span>Type in your e-mail address in the field below</span>
+          <input type="email" placeholder="Email">
+          <button>Submit</button>
+        </form>
+      `;
+    });
+  });
